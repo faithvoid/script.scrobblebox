decdd068fab28d9325bcd4861e719a5cf8d338ac
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-11-12 11:47:39 -0500

    Create default.py

---
+++ default.py
@@ -0,0 +1,23 @@
+import xbmc
+import xbmcgui
+import xml.etree.ElementTree as ET
+import sys
+
+def main():
+    dialog = xbmcgui.Dialog()
+    feeds = [
+        ("Start ScrobbleBox", "RunScript(Q:\\scripts\\ScrobbleBox\\scrobblebox.py)"),
+        ("Settings", "RunScript(Q:\\scripts\\ScrobbleBox\\settings.py)"),
+        ("Enable/Disable On Startup", "RunScript(Q:\\scripts\\ScrobbleBox\\autoexec-scrobblebox.py)"),
+    ]
+    
+    feed_list = [name for name, _ in feeds]
+    selected = dialog.select(u"ScrobbleBox", feed_list)
+    
+    if selected >= 0:
+        name, url = feeds[selected]
+        if "RunScript" in url:
+            xbmc.executebuiltin(url)
+
+if __name__ == '__main__':
+    main()
